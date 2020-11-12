acc62a5cfbd9f7dbfac1c9aa47f3c39bfa8f9618
airtonsm <airtonsmorais28@gmail.com>
2020-11-11 23:00:50 -0300

    Studies in Spring boot

--- src/main/java/com/devsuperior/myfirstproject/entities/Product.java
+++ src/main/java/com/devsuperior/myfirstproject/entities/Product.java
@@ -1,27 +1,27 @@
 package com.devsuperior.myfirstproject.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
-public class Category implements Serializable{
+public class Product implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
 	private String name;
+	private Double price;
 	
-	@JsonIgnore
-	private List<Product> products = new ArrayList<>();	
+	private Category category;
 	
-	public Category() {
+	
+	public Product() {
 		
-	}	
-	
-	public Category(Long id, String name) {
+	}
+
+	public Product(Long id, String name, Double price, Category category) {
 		this.id = id;
 		this.name = name;
+		this.price = price;
+		this.category = category;
 	}
 
 	public Long getId() {
@@ -31,6 +31,7 @@ public class Category implements Serializable{
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 
 	public String getName() {
 		return name;
@@ -38,21 +39,34 @@ public class Category implements Serializable{
 
 	public void setName(String name) {
 		this.name = name;
-	}
-	
-	
+	}		
 
-	public List<Product> getProducts() {
-		return products;
+
+	public Double getPrice() {
+		return price;
 	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}	
 
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((category == null) ? 0 : category.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
 		return result;
 	}
 
@@ -64,7 +78,12 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
+		if (category == null) {
+			if (other.category != null)
+				return false;
+		} else if (!category.equals(other.category))
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -75,10 +94,13 @@ public class Category implements Serializable{
 				return false;
 		} else if (!name.equals(other.name))
 			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
 		return true;
 	}
-	
-	
 	
 	
 
